804ae2d465fa09e7194b1b23917a613ce82112d9
Alex Hsu <a0970396551.01@gamil.com>
2022-04-08 17:04:56 +0800

    n is 35

--- src/test/java/com/example/practice/leetcode/SolutionTest.java
+++ src/test/java/com/example/practice/leetcode/SolutionTest.java
@@ -41,4 +41,19 @@ class SolutionTest {
                 "19","Buzz","FizzAlex"
         ), new Solution().fizzBuzz(21));
     }
+
+    @Test
+    void n_is_35() {
+        assertEquals(List.of("1","2","Fizz","4","Buzz","Fizz",
+                "Alex","8","Fizz","Buzz","11","Fizz",
+                "13","Alex","FizzBuzz",
+                "16","17","Fizz",
+                "19","Buzz","FizzAlex",
+                "22","23","Fizz",
+                "Buzz","26","Fizz",
+                "Alex","29","FizzBuzz",
+                "31","32","Fizz",
+                "34","BuzzAlex"
+        ), new Solution().fizzBuzz(35));
+    }
 }
