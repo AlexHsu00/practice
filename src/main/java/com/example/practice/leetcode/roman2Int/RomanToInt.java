d337afb7925bf7a482a736a4d9cc59149645bafe
AlexHsu <a0970396551@gmail.com>
2022-05-06 16:41:44 +0800

    solution

--- src/main/java/com/example/practice/leetcode/roman2Int/RomanToInt.java
+++ src/main/java/com/example/practice/leetcode/roman2Int/RomanToInt.java
@@ -1,8 +1,123 @@
 package com.example.practice.leetcode.roman2Int;
 
-public class RomanToInt {
-    public int romanToInt(String s) {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
-        return 3;
+public class RomanToInt {
+
+    public int romanToIntV1(String s) {
+        return romanCalculatorV1(s, createRomanMapperV1());
+    }
+
+    private int romanCalculatorV1(String roman, Map<String, Integer> mapper) {
+        List<String> subtractLetter = List.of("I", "X", "C");
+        int result = 0;
+        char[] charOfRomans = roman.toCharArray();
+        for (int i = 0; i < charOfRomans.length; i++) {
+            if (i < charOfRomans.length - 1 && subtractLetter.contains(String.valueOf(charOfRomans[i]))) {
+                String subtractRoman = String.valueOf(charOfRomans[i]) + charOfRomans[i + 1];
+                if (Objects.requireNonNullElse(mapper.get(subtractRoman), 0) > 0) {
+                    result += mapper.get(subtractRoman);
+                    i++;
+                    continue;
+                }
+            }
+            result += mapper.get(String.valueOf(charOfRomans[i]));
+
+        }
+        return result;
+    }
+
+    private Map<String, Integer> createRomanMapperV1() {
+        Map<String, Integer> roman2IntegerMapper = new HashMap<>();
+        roman2IntegerMapper.put("I", 1);
+        roman2IntegerMapper.put("V", 5);
+        roman2IntegerMapper.put("X", 10);
+        roman2IntegerMapper.put("L", 50);
+        roman2IntegerMapper.put("C", 100);
+        roman2IntegerMapper.put("D", 500);
+        roman2IntegerMapper.put("M", 1000);
+        roman2IntegerMapper.put("IV", 4);
+        roman2IntegerMapper.put("IX", 9);
+        roman2IntegerMapper.put("XL", 40);
+        roman2IntegerMapper.put("XC", 90);
+        roman2IntegerMapper.put("CD", 400);
+        roman2IntegerMapper.put("CM", 900);
+        return roman2IntegerMapper;
+    }
+
+    public int romanToIntV2(String s) {
+        return romanCalculatorV2(s, createRomanMapperV2());
+    }
+
+    private int romanCalculatorV2(String s, Map<String, Integer> mapper) {
+        int result = 0;
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (i < chars.length - 1 && (mapper.get(String.valueOf(chars[i])) < mapper.get(String.valueOf(chars[i + 1])))) {
+                String s1 = String.valueOf(chars[i]) + chars[i + 1];
+                result += (mapper.get(String.valueOf(chars[i + 1])) - mapper.get(String.valueOf(chars[i])));
+                i++;
+                continue;
+            }
+            result += mapper.get(String.valueOf(chars[i]));
+        }
+        return result;
+    }
+
+    private Map<String, Integer> createRomanMapperV2() {
+        Map<String, Integer> roman2IntegerMapper = new HashMap<>();
+        roman2IntegerMapper.put("I", 1);
+        roman2IntegerMapper.put("V", 5);
+        roman2IntegerMapper.put("X", 10);
+        roman2IntegerMapper.put("L", 50);
+        roman2IntegerMapper.put("C", 100);
+        roman2IntegerMapper.put("D", 500);
+        roman2IntegerMapper.put("M", 1000);
+        return roman2IntegerMapper;
+    }
+
+    public int romanToIntV3(String s) {
+        return romanCalculatorV3(s, createRomanMapperV3());
+    }
+
+    private int romanCalculatorV3(String s, Map<String, Integer> mapper) {
+        List<String> ixc = List.of("I", "X", "C");
+        int result = 0;
+        String[] sp = s.split("");
+
+        for (int i = 0; i < sp.length; i++) {
+            if (i < sp.length - 1 && ixc.contains(sp[i])) {
+                String s1 = sp[i] + sp[i + 1];
+                if (Objects.requireNonNullElse(mapper.get(s1), 0) > 0) {
+                    result += mapper.get(s1);
+                    i++;
+                    continue;
+                }
+            }
+            result += mapper.get(sp[i]);
+        }
+        return result;
+    }
+
+    private Map<String, Integer> createRomanMapperV3() {
+        Map<String, Integer> roman2IntegerMapper = new HashMap<>();
+        roman2IntegerMapper.put("I", 1);
+        roman2IntegerMapper.put("V", 5);
+        roman2IntegerMapper.put("X", 10);
+        roman2IntegerMapper.put("L", 50);
+        roman2IntegerMapper.put("C", 100);
+        roman2IntegerMapper.put("D", 500);
+        roman2IntegerMapper.put("M", 1000);
+        roman2IntegerMapper.put("IV", 4);
+        roman2IntegerMapper.put("IX", 9);
+        roman2IntegerMapper.put("XL", 40);
+        roman2IntegerMapper.put("XC", 90);
+        roman2IntegerMapper.put("CD", 400);
+        roman2IntegerMapper.put("CM", 900);
+        return roman2IntegerMapper;
     }
 }
--- src/test/java/com/example/practice/leetcode/roman2Int/RomanToIntTest.java
+++ src/test/java/com/example/practice/leetcode/roman2Int/RomanToIntTest.java
@@ -1,32 +1,35 @@
 package com.example.practice.leetcode.roman2Int;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RomanToIntTest {
-
-//    Example 1:
-//
-//    Input: s = "III"
-//    Output: 3
-//    Explanation: III = 3.
-//    Example 2:
-//
-//    Input: s = "LVIII"
-//    Output: 58
-//    Explanation: L = 50, V= 5, III = 3.
-//    Example 3:
-//
-//    Input: s = "MCMXCIV"
-//    Output: 1994
-//    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+    private RomanToInt romanToInt = new RomanToInt();
+    @Test
+    void when_input_is_III() {
+        assertEquals(3, romanToInt.romanToIntV1("III"));
+        assertEquals(3, romanToInt.romanToIntV2("III"));
+        assertEquals(3, romanToInt.romanToIntV3("III"));
+    }
 
     @Test
-    void when_input_is_iii() {
-        RomanToInt romanToInt = new RomanToInt();
-        assertEquals(3, romanToInt.romanToInt("III"));
+    void when_input_is_LVIII() {
+        assertEquals(58, romanToInt.romanToIntV1("LVIII"));
+        assertEquals(58, romanToInt.romanToIntV2("LVIII"));
+        assertEquals(58, romanToInt.romanToIntV3("LVIII"));
+    }
+
+    @Test
+    void when_input_is_MCMXCIV() {
+        assertEquals(1994, romanToInt.romanToIntV1("MCMXCIV"));
+        assertEquals(1994, romanToInt.romanToIntV2("MCMXCIV"));
+        assertEquals(1994, romanToInt.romanToIntV3("MCMXCIV"));
     }
 }
